da630ab7d5ddae3687a6e387f4f8c1a381d40e8d
MohammadHeydari <heydari.mohammad8@gmail.com>
2026-05-26 23:42:55 +0330

    updated connection to neo4j file

--- test_neo4j_.py
+++ test_neo4j_.py
@@ -2,7 +2,7 @@ from neo4j import GraphDatabase
 
 driver = GraphDatabase.driver(
     "bolt://localhost:7687",
-    auth=("neo4j", "add_your_neo4j_password") # add_your_neo4j_password
+    auth=("neo4j", "add_your_neo4j_password_here") # add_your_neo4j_password_here
 )
 
 with driver.session() as session:
